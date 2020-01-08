c8d6e123662709b34f176f21ec08b416f4b3ce97
Abourass <abourassa@assetval.com>
2020-01-08 14:30:38 -0700

    First commit

---
+++ src/wizard.mjs
@@ -0,0 +1,32 @@
+const Wizard = function(className, props) {
+  const classes = {
+    [className]: class {
+    constructor() {
+      this._state = (props.state) ? props.state : null;
+    }
+
+    static get observedAttributes() {return [...props.state]}
+
+    attributeChangedCallback(name, oldValue, newValue){
+      this._state = newValue;
+      this._updateRendering();
+    }
+    connectedCallback(){ this._updateRendering() }
+
+    get state(){
+      return this._state;
+    }
+    set state(newState){
+      this.setAttribute('state', newState)
+    }
+
+    _updateRendering(){
+
+    }
+  }};
+  return new classes[className];
+};
+
+const sample = new Wizard('superspan', {state: {clicked: false}});
+console.log(sample);
+
