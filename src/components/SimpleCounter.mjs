6924bbe69bed3c386d608168b5ce4603ac079e7b
Abourass <abourassa@assetval.com>
2020-01-08 15:03:39 -0700

    Make it so number two

--- src/components/SimpleCounter.mjs
+++ src/components/SimpleCounter.mjs
@@ -1,10 +1,10 @@
 import {html, define} from 'https://unpkg.com/hybrids@4.0.4/src';
 
 export function increaseCount(host){ host.count += 1; }
-
+const texty = () => html`<span>Hello</span>`
 export const SimpleCounter = {
   count: 0,
-  render: ({count}) => html`<button onclick="${increaseCount}">Count: ${count}</button>`
+  render: ({count}) => html`<button onclick="${increaseCount}">Count: ${count} ${texty()}</button>`
 };
 
 define('simple-counter', SimpleCounter);
